519019b444d3f8d077b75b04211b6895be6aa867
L-Trump <yinghaochi@163.com>
2023-08-07 13:25:37 +0800

    Ready for clean submodules

--- dotfiles/nvim/lua/modules/configs/custom/distant.lua
+++
@@ -1,5 +0,0 @@
-return function ()
-    require('distant').setup({
-        ['*'] = require('distant.settings').chip_default();
-    })
-end
--- dotfiles/nvim/lua/modules/configs/custom/wrapping.lua
+++
@@ -1,6 +0,0 @@
-return function ()
-    require("wrapping").setup({
-        create_keymappings = false,
-        softener = { tex = true },
-    })
-end
