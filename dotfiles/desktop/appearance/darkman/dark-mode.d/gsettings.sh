f5401a456eccd0b59e8dafb53d259bc38cd39339
L-Trump <yinghaochi@163.com>
2024-05-24 00:40:33 +0800

    Wayland as main display manager

--- dotfiles/desktop/appearance/darkman/dark-mode.d/gsettings.sh
+++ dotfiles/desktop/appearance/darkman/dark-mode.d/gsettings.sh
@@ -2,6 +2,6 @@
 cur_theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
 cur_theme=${cur_theme#"'"}
 cur_theme=${cur_theme%"'"}
-if [ -n "$(echo $cur_theme  | grep -E '^.*-dark')" ]; then
-	gsettings set org.gnome.desktop.interface gtk-theme ${cur_theme%"-dark"}
+if [ -z "$(echo $cur_theme  | grep -E '^.*-dark')" ]; then
+	gsettings set org.gnome.desktop.interface gtk-theme "${cur_theme}-dark"
 fi
---
+++ dotfiles/desktop/waybar/rofi-menus/powermenu.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+dir="~/.config/waybar/rofi-menus/rofi"
+uptime=$(uptime -p | sed -e 's/up //g')
+DESKTOP_SESSION="i3"
+rofi_command="rofi -dpi 144 -theme $dir/powermenu.rasi"
+
+# Options
+shutdown=" Shutdown"
+reboot=" Restart"
+lock=" Lock"
+suspend=" Sleep"
+logout=" Logout"
+displayoff=" DisplayOff"
+
+# Confirmation
+confirm_exit() {
+	rofi -dpi 144 -dmenu\
+		-i\
+		-no-fixed-num-lines\
+		-p "Are You Sure? : "\
+		-theme $dir/confirm.rasi
+}
+
+# Message
+msg() {
+	rofi -dpi 144 -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+}
+
+# Variable passed to rofi
+options="$lock\n$displayoff\n$suspend\n$logout\n$reboot\n$shutdown"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -l 6 -selected-row 0)"
+case $chosen in
+    $shutdown)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl poweroff
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $reboot)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl reboot
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $lock)
+	        if [[ -f /usr/bin/myi3lock ]]; then
+			swaylock
+	        elif [[ -f /usr/bin/myi3lock ]]; then
+			myi3lock
+		elif [[ -f /usr/bin/i3lock ]]; then
+			i3lock
+		elif [[ -f /usr/bin/betterlockscreen ]]; then
+			betterlockscreen -l
+		fi
+        ;;
+    $displayoff)
+		sleep 0.1
+		pkill -USR1 swayidle
+        ;;
+    $suspend)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $logout)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+				i3-msg exit
+			elif [[ "$DESKTOP_SESSION" == "sway" ]]; then
+				swaymsg exit
+			fi
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+esac
